94451ecc85edfd700a696eb48de1bdda58d78466
meesh <meeshoff2006@gmail.com>
2025-06-08 14:46:24 +0200

    Changed like to favorite and added notes to HotspotList.js

--- components/HotspotList.js
+++ components/HotspotList.js
@@ -1,13 +1,15 @@
 import {useContext, useEffect, useState} from "react";
 import {
     ActivityIndicator,
-    Animated,
+    Button,
     FlatList,
+    Modal,
     Pressable,
     SafeAreaView,
+    ScrollView,
     StyleSheet,
     Text,
-    useAnimatedValue,
+    TextInput,
     View
 } from "react-native";
 import {SafeAreaProvider} from "react-native-safe-area-context";
@@ -21,18 +23,14 @@ export default function HotspotList() {
     const [hotspot, setHotspot] = useState([])
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(null)
-    const [likes, setLikes] = useState({})
+    const [favorites, setfavorites] = useState({})
+    const [notes, setNotes] = useState({})
+    const [modalVisible, setModalVisible] = useState(false)
+    const [currentHotspotId, setCurrentHotspotId] = useState(null)
+    const [noteInput, setNoteInput] = useState('')
+
     const {darkMode} = useContext(Theme)
-
-    const fadeAnim = useAnimatedValue(0);
-
-    const fadeIn = () => {
-        Animated.timing(fadeAnim, {
-            toValue: 1,
-            duration: 5000,
-            useNativeDriver: true,
-        }).start();
-    };
+    const image = {uri: 'https://rotterdammakeithappen.nl/app/uploads/2019/09/201908_VerwoesteStad_558A3033_IrisvandenBroek-855x570.jpg'};
 
     const fetchSpots = async () => {
         try {
@@ -47,37 +45,71 @@ export default function HotspotList() {
         }
     }
 
-    const loadLikes = async () => {
+    const loadFavorites = async () => {
         try {
-            const storedLikes = await AsyncStorage.getItem('likes')
-            if (storedLikes) {
-                setLikes(JSON.parse(storedLikes))
+            const storedfavorites = await AsyncStorage.getItem('favorites')
+            if (storedfavorites) {
+                setfavorites(JSON.parse(storedfavorites))
             }
         } catch (error) {
-            console.error('Fout bij het ophalen van likes:', error)
+            console.error('Fout bij het ophalen van favorites:', error)
         }
     }
 
-    const toggleLike = async (hotspotId) => {
+    const toggleFavorite = async (hotspotId) => {
         try {
-            const updatedLikes = {...likes}
+            const updatedfavorites = {...favorites}
 
-            if (updatedLikes[hotspotId]) {
-                delete updatedLikes[hotspotId]
+            if (updatedfavorites[hotspotId]) {
+                delete updatedfavorites[hotspotId]
             } else {
-                updatedLikes[hotspotId] = true
+                updatedfavorites[hotspotId] = true
             }
 
-            setLikes(updatedLikes)
+            setfavorites(updatedfavorites)
         } catch (error) {
-            console.error('Fout bij het opslaan van likes:', error)
+            console.error('Fout bij het opslaan van favorites:', error)
         }
+    }
+
+    const loadNotes = async () => {
+        try {
+            const storedNotes = await AsyncStorage.getItem('notes')
+            if (storedNotes) {
+                setNotes(JSON.parse(storedNotes))
+            }
+        } catch (error) {
+            console.error('Fout bij het ophalen van notes:', error)
+        }
+    }
+
+    const addNote = async (hotspotId, note) => {
+        try {
+            const updatedNotes = {...notes, [hotspotId]: note}
+            setNotes(updatedNotes)
+            await AsyncStorage.setItem('notes', JSON.stringify(updatedNotes))
+        } catch (error) {
+            console.error('Fout bij het opslaan van notes:', error)
+        }
+    }
+
+    const openNoteModal = (hotspotId) => {
+        setCurrentHotspotId(hotspotId)
+        setNoteInput(notes[hotspotId] || '')
+        setModalVisible(true)
+    }
+
+    const saveNote = () => {
+        addNote(currentHotspotId, noteInput)
+        setModalVisible(false)
     }
 
     useEffect(() => {
         fetchSpots()
 
-        loadLikes()
+        loadFavorites()
+
+        loadNotes()
     }, [])
 
     if (loading === true) {
@@ -112,9 +144,9 @@ export default function HotspotList() {
             color: darkMode ? 'white' : 'black'
         },
 
-        like: {
-            backgroundColor: likes ? 'white' : 'black'
-        }
+        favorite: {
+            backgroundColor: favorites ? 'white' : 'black'
+        },
 
     });
 
@@ -131,16 +163,71 @@ export default function HotspotList() {
                                   className="p-6 m-6 border-2 border-gray-300 rounded-lg shadow-xl"
 
                             >
-                                <Text style={styles.listTextTitle} className="font-bold text-lg">{item.name}</Text>
-                                <Text style={styles.listTextType}
-                                      className="font-semibold pt-2 pb-2 color-blue-500">{item.type}</Text>
-                                <Text style={styles.listTextDescription} className="">{item.description}</Text>
-                                <Pressable onPress={() => toggleLike(item.id)}
-                                           className={`p-2 mt-4 rounded max-w-20 flex justify-center items-center ${likes[item.id] ? `bg-[#1a1110] ${darkMode ? 'border-2 border-gray-300' : 'border-1'}` : 'bg-[#f5f5f5]'}`}>
-                                    <Text
-                                        className="color-gray-100">{likes[item.id] ? '❤️' : '🤍'}</Text>
-                                </Pressable>
+                                <View className="flex bg-gray-100 rounded-lg p-4">
+                                    <Text style={styles.listTextTitle} className="font-bold text-lg">{item.name}</Text>
+                                    <Text style={styles.listTextDescription} className="">{item.description}</Text>
+                                    <View
+                                        className="flex flex-row justify-center items-center rounded-full max-w-28 bg-sky-100 mt-4">
+                                        <Text style={styles.listTextType}
+                                              className="font-semibold p-2 color-blue-500 ">{item.type}</Text>
+                                    </View>
+                                </View>
+                                <View className='flex flex-row-reverse justify-between items-center'>
+                                    {/*FAVORIET:*/}
+                                    <Pressable onPress={() => toggleFavorite(item.id)}
+                                               className={`p-2 mt-4 w-16 h-16 rounded-full flex justify-center items-center ${favorites[item.id] ? `bg-blue-500` : 'bg-[#f5f5f5]'}`}>
+                                        <Text
+                                            className="color-gray-100">{favorites[item.id] ? '❤️' : '🤍'}</Text>
+                                    </Pressable>
+
+                                    {/*NOTITIE*/}
+                                    <Pressable onPress={() => openNoteModal(item.id)}
+                                               className="p-6 mt-4 w-72 max-w-72 border-2 border-gray-500 rounded-lg bg-white">
+                                        <View>
+                                            <Text
+                                                className={`${darkMode ? 'text-white' : 'text-black'} font-bold`}>
+                                                Notitie:
+                                            </Text>
+                                            <ScrollView>
+                                                <Text
+                                                    className={`${darkMode ? 'text-white' : 'text-black'}`}
+                                                >
+                                                    {notes[item.id] || ''}
+                                                </Text>
+                                            </ScrollView>
+                                        </View>
+                                    </Pressable>
+                                </View>
+
+
                             </View>
+                            <Modal
+                                animationType="slide"
+                                transparent={true}
+                                visible={modalVisible}
+                                onRequestClose={() => {
+                                    setModalVisible(false);
+                                }}
+                            >
+                                <View className="flex-1 justify-center items-center bg-black/30">
+                                    <View
+                                        className={`p-6 flex flex-col justify-center items-center ${darkMode ? 'bg-gray-800' : 'bg-white'} w-80 border-2 border-gray-500 rounded-lg`}
+                                    >
+                                        <TextInput
+                                            placeholder="Notitie..."
+                                            value={noteInput}
+                                            onChangeText={setNoteInput}
+                                            maxLength={100}
+                                            className={`border-2 border-gray-500 rounded-lg p-4 mb-6 ${darkMode ? 'bg-white' : 'bg-white'} w-72`}
+                                        />
+                                        <Text
+                                            className={`${darkMode ? 'text-gray-300' : 'text-gray-500'} text-right`}>
+                                            {noteInput.length}/100 tekens
+                                        </Text>
+                                        <Button title="Opslaan" onPress={saveNote}/>
+                                    </View>
+                                </View>
+                            </Modal>
                         </Pressable>
                     )}
 
--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,12 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
+const {getDefaultConfig} = require("expo/metro-config");
+const {withNativeWind} = require('nativewind/metro');
+const {wrapWithReanimatedMetroConfig} = require("react-native-reanimated/metro-config");
 
-const config = getDefaultConfig(__dirname)
 
-module.exports = withNativeWind(config, { input: './global.css' })
+const baseConfig = getDefaultConfig(__dirname)
+
+const nativeWindConfig = withNativeWind(baseConfig, {input: "./global.css"});
+
+const finalConfig = wrapWithReanimatedMetroConfig(nativeWindConfig);
+
+module.exports = finalConfig;
