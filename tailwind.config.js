c7ce2d6a4fcbf8d2b48a62f61b17ecd7622cd09f
meesh <meeshoff2006@gmail.com>
2025-07-26 14:17:07 +0200

    Updated Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 // NOTE: Elke file met met code voor de mobile app moet aan content toegevoegd worden
 module.exports = {
-  content: ["./components/**/*.{js,jsx,ts,tsx}", "./HotspotsList.js", "./Homepage.js", "./Settings.js"],
-  presets: [require("nativewind/preset")],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./src/components/**/*.{js,jsx,ts,tsx}",
+        "./src/pages/**/*.{js,jsx,ts,tsx}",
+        "./src/routes/**/*.{js,jsx,ts,tsx}",
+    ],
+    presets: [require("nativewind/preset")],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
 
